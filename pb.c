1e8b13556ba45ed67542f46859762686ac233c4c
YESVANTHKRISHNA.V <114081669+yesvanthkrishna16@users.noreply.github.com>
2024-06-14 10:30:17 +0530

    Update pb.c

--- pb.c
+++ pb.c
@@ -1,11 +1,13 @@
+//this program is for control a LED with a push button
 #include<pic.h>
+__CONFIG(0x3f3a);//use this line before converting this code to hex code.
 void main()
 {
-	TRISB=0XFE;
-	RB0=0;
+	TRISB=0XFE;//initilizing the port B
+	RB0=0;//LED has been connected 
 	while(1)
 	{
-		if(RB1==1)
+		if(RB1==1)//RB1 push button connected
 		{
 			RB0=1;
 		}
